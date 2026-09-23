cb1aef69ff293d5f10d89dd08617bedb3d4704f4
Makham-Cha <opuan17@gmail.com>
2026-09-23 11:52:06 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
   LIFF_ID: '2011672004-mTPUoEBy',
-  API_URL: 'https://script.google.com/macros/s/AKfycbzB1mUqqbgQnqBH6SxqucLzVAJxh-s_ebrqgP1YooPUueMzVxnyxAM7tKsWfRJrTs_i1Q/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyry3Gdk4lUUjGsiVPzCPORyLYiK0OWNxgCtlVJYcnGJvJUM4vCalkN8pgtNktyCejfAA/exec',
   SHOP_NAME: 'Makham Cha',
   CONTACT_TEXT: 'ติดต่อร้านผ่าน LINE OA'
 };
